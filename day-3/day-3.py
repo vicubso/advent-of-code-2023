ff0392f6f7f13675f17f78e617c5fb70ff684d04
vicubso <vicubso@gmail.com>
2023-12-07 23:40:58 +0100

    Day 3, part 2

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -55,3 +55,46 @@ print(f"The answer to part 1 is {part_1('input.txt')}")
 
 # %% [markdown]
 # ## Part 2
+
+# %%
+filename = "input.txt"
+
+def part_2(filename):
+    with open(filename) as f:
+        symbols = ''.join(set(re.findall(r"[^0-9\n.]", f.read())))
+
+    with open("input.txt") as f:
+        matrix =  np.array([list(x) for x in f.read().splitlines()])
+    # include a frame of "." around the matrix
+    matrix = np.pad(matrix, 1, 'constant', constant_values=".")
+
+    def get_current_word(matrix, i, j):
+        # Get the word (made of digits) that overlaps with the current position
+        if not re.findall(f"\d", matrix[i][j]): # If the current position is not a digit
+            return ''
+        j_start = j
+        j_end = j
+        while re.findall(r"\d", matrix[i][j_start-1]):
+            j_start -= 1
+        while re.findall(r"\d", matrix[i][j_end+1]):
+            j_end += 1
+        current_word = ''.join(matrix[i][j_start:j_end+1])
+        current_word = current_word + f"-{i},{j_start}" # Append i,j_start to make the word unique 
+        return current_word
+
+    m = matrix.shape[0]
+    n = matrix.shape[1]
+    cum_sum = 0
+    for i in range(1,m-1):
+        for j in range(1,n-1):
+            if matrix[i][j] == "*":
+                adjacent_indices = np.array([[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1,-1], [1,0], [1,1]]) + [i,j]
+                adjacent_words = list(set([get_current_word(matrix, x[0], x[1]) for x in adjacent_indices]) - {''})
+                if len(adjacent_words) == 2:
+                    adjacent_words = [int(x.split("-")[0]) for x in adjacent_words] # Remove the i,j_start part
+                    print(f"{i},{j}: {adjacent_words}")
+                    cum_sum += np.prod(adjacent_words)
+
+    return cum_sum
+
+print(f"The answer to part 2 is {part_2('input.txt')}")
