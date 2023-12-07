2df37724d8304d1ec9207e956cf657ba31389b36
vicubso <vicubso@gmail.com>
2023-12-07 20:17:45 +0100

    Day 3 part 1

---
+++ day-3/day-3.py
@@ -0,0 +1,53 @@
+# %% [markdown]
+# # AoC 2023, day 3
+
+# %% [markdown]
+# ## Part 1
+
+# %%
+import re
+import numpy as np
+
+# %%
+# Find all symbols that are not numbers, newlines or dots. Ignore repetitions
+with open("input.txt") as f:
+    symbols = ''.join(set(re.findall(r"[^0-9\n.]", f.read())))
+
+# %%
+# Read the input as a matrix, an replace all symbols with "*"
+with open("input.txt") as f:
+    matrix =  np.array([list(x) for x in re.sub(r"[{}]+".format(re.escape(symbols)), "*", f.read()).splitlines()])
+# include a frame of "." around the matrix
+matrix = np.pad(matrix, 1, 'constant', constant_values=".")
+
+# %%
+# Find the positions of all "*" symbols
+locations = np.argwhere(matrix == "*")
+
+# Create a matrix with ones at the 8 positions around each "*", zeros elsewhere
+ones_zeros = np.zeros_like(matrix, dtype=int)
+for loc in locations:
+    row, col = loc
+    ones_zeros[row-1:row+2, col-1:col+2] = 1
+
+# %%
+# Find all the numbers in matrix that overlap with at least one 1 in ones_zeros
+cum_sum = 0
+m = matrix.shape[0]
+n = matrix.shape[1]
+for i in range(m):
+    row = matrix[i]
+    j = 0
+    while j < n:
+        current_idx = np.array([], dtype=int)
+        while row[j] != "*" and row[j] != "." and i < n:
+            current_idx = np.append(current_idx, j)
+            j += 1
+        current_word = ''.join(row[current_idx])
+        if 1 in ones_zeros[i][current_idx]:
+            cum_sum += int(current_word)
+        j += 1
+print(f"The answer to part 1 is {cum_sum}")
+
+# %% [markdown]
+# ## Part 2
